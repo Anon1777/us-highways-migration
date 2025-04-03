9994993fa2a8def8e56c7ce2cc8402e70e2c498a
farrelj5 <farrelj5@tcnj.edu>
2025-04-03 19:13:36 -0400

    incl java oopsies

---
+++ static/scripts.js
@@ -0,0 +1,2 @@
+var div = document.getElementById("list-redirects");
+var redirects = div.querySelectorAll("a");
--- static/top.php
+++ static/top.php
@@ -5,13 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>US Highway System - US-1</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/static/style.css">
 </head>
 <body>
     <div class="nav">
-        <nav class="navbar navbar-expand-lg">
+        <nav class="navbar fixed-top navbar-expand-lg">
             <div class="container-fluid">
-                <a class="navbar-brand" href="/pages/index.html"><img class="branded-img" src="/static/img/us_blank.png"></a>
+                <a class="navbar-brand" href="/pages/index.html"><img class="branded-img" src="/static/img/US_blank.svg.png"></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -179,5 +179,5 @@
             </div>
         </nav>
     </div>
-    <div class="header">
+    <div class="container row" id="list-redirects">
         
