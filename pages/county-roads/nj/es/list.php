071b5021d9f2c8aee2d8cd2b957d7ff6e387d053
farrelj5 <farrelj5@tcnj.edu>
2025-06-11 00:21:51 -0400

    essex #19

--- pages/county-roads/nj/es/list.php
+++ pages/county-roads/nj/es/list.php
@@ -4,6 +4,84 @@
             <h6 class="dir"><a href="../../../index.php">Home</a> > <a href="../list.php">County Roads in New Jersey</a> > County Roads in Essex County, New Jersey</h6>
             <h1 class="header">List of County Roads in Essex County, New Jersey</h1>
             <h3 class="content text-dark"><a href="601.php">Essex CR 601 <i class="text-body-tertiary">Chancellor Ave</i></a></h3>
+            <h3 class="content text-dark"><a href="602.php">Essex CR 602 <i class="text-body-tertiary">Lyons Ave</i></a></h3>
+            <h3 class="content text-dark"><a href="603.php">Essex CR 603 <i class="text-body-tertiary">Springfield Ave</i></a></h3>
+            <h3 class="content text-dark"><a href="604.php">Essex CR 604 <i class="text-body-tertiary">Lindlsey Rd</i></a></h3>
+            <h3 class="content text-dark"><a href="605.php">Essex CR 605 <i class="text-body-tertiary">Sanford Ave, Sanford St</i></a></h3>
+            <h3 class="content text-dark"><a href="606.php">Essex CR 606 <i class="text-body-tertiary">Parsonage Hill Rd</i></a></h3>
+            <h3 class="content text-dark"><a href="607.php">Essex CR 607 <i class="text-body-tertiary">Passaic Ave, Walnut St</i></a></h3>
+            <h3 class="content text-dark"><a href="608.php">Essex CR 608 <i class="text-body-tertiary">Hobart Gap Rd, White Oak Ridge Rd, E Hobart Gap Rd, W Hobart Gap Rd</i></a></h3>
+            <h3 class="content text-dark"><a href="609.php">Essex CR 609 <i class="text-body-tertiary">Eisenhower Pkwy</i></a></h3>
+            <h3 class="content text-dark"><a href="610.php">Essex CR 610 <i class="text-body-tertiary">Old Mt. PLeasant Ave</i></a></h3>
+            <h3 class="content text-dark"><a href="611.php">Essex CR 611 <i class="text-body-tertiary">Eagle Rock Ave</i></a></h3>
+            <h3 class="content text-dark"><a href="613.php">Essex CR 613 <i class="text-body-tertiary">Passaic Ave, Two Bridges Rd</i></a></h3>
+            <h3 class="content text-dark"><a href="613s.php">Essex CR 613 Spur <i class="text-body-tertiary">Kirkpatrick Ln</i></a></h3>
+            <h3 class="content text-dark"><a href="614.php">Essex CR 614 <i class="text-body-tertiary">Clinton Rd</i></a></h3>
+            <h3 class="content text-dark"><a href="614s.php">Essex CR 614 Spur <i class="text-body-tertiary">Brook St</i></a></h3>
+            <h3 class="content text-dark"><a href="615.php">Essex CR 615 <i class="text-body-tertiary">Fairfield Rd, Little Falls Rd</i></a></h3>
+            <h3 class="content text-dark"><a href="616.php">Essex CR 616 <i class="text-body-tertiary">Stevens Ave</i></a></h3>
+            <h3 class="content text-dark"><a href="617.php">Essex CR 617 <i class="text-body-tertiary">Little Falls Rd</i></a></h3>
+            <h3 class="content text-dark"><a href="618.php">Essex CR 618 <i class="text-body-tertiary">Reservoir Dr, Normal Ave</i></a></h3>
+            <h3 class="content text-dark"><a href="619.php">Essex CR 619 <i class="text-body-tertiary">Hillside Ave, Mill Rd, Stuyvesant Ave</i></a></h3>
+            <h3 class="content text-dark"><a href="620.php">Essex CR 620 <i class="text-body-tertiary">Upper Mountain Ave</i></a></h3>
+            <h3 class="content text-dark"><a href="621.php">Essex CR 621 <i class="text-body-tertiary">Valley Rd</i></a></h3>
+            <h3 class="content text-dark"><a href="622.php">Essex CR 622 <i class="text-body-tertiary">W Passaic Ave, Darling Ave</i></a></h3>
+            <h3 class="content text-dark"><a href="623.php">Essex CR 623 <i class="text-body-tertiary">Grove St</i></a></h3>
+            <h3 class="content text-dark"><a href="624.php">Essex CR 624 <i class="text-body-tertiary">River Rd</i></a></h3>
+            <h3 class="content text-dark"><a href="625.php">Essex CR 625 <i class="text-body-tertiary">Hollywood Ave</i></a></h3>
+            <h3 class="content text-dark"><a href="626.php">Essex CR 626 <i class="text-body-tertiary">Horseneck Rd</i></a></h3>
+            <h3 class="content text-dark"><a href="627.php">Essex CR 627 <i class="text-body-tertiary">Plymouth St</i></a></h3>
+            <h3 class="content text-dark"><a href="628.php">Essex CR 628 <i class="text-body-tertiary">W Greenbrook Rd</i></a></h3>
+            <h3 class="content text-dark"><a href="629.php">Essex CR 629 <i class="text-body-tertiary">Pier Ln</i></a></h3>
+            <h3 class="content text-dark"><a href="630.php">Essex CR 630 <i class="text-body-tertiary">Millburn Ave</i></a></h3>
+            <h3 class="content text-dark"><a href="630a.php">Essex CR 630 Alt <i class="text-body-tertiary">Vauxhall Rd</i></a></h3>
+            <h3 class="content text-dark"><a href="631.php">Essex CR 631 <i class="text-body-tertiary">Central Ave, Grandview Ave</i></a></h3>
+            <h3 class="content text-dark"><a href="632.php">Essex CR 632 <i class="text-body-tertiary">Westville Ave</i></a></h3>
+            <h3 class="content text-dark"><a href="633.php">Essex CR 633 <i class="text-body-tertiary">Runnymede Rd, Lane Ave</i></a></h3>
+            <h3 class="content text-dark"><a href="634.php">Essex CR 634 <i class="text-body-tertiary">Laurel Ave</i></a></h3>
+            <h3 class="content text-dark"><a href="635.php">Essex CR 635 <i class="text-body-tertiary">Shrewsbury Dr</i></a></h3>
+            <h3 class="content text-dark"><a href="636.php">Essex CR 636 <i class="text-body-tertiary">Pleasant Valley Way, Lakeview Ave</i></a></h3>
+            <h3 class="content text-dark"><a href="637.php">Essex CR 637 <i class="text-body-tertiary">Fairview Ave</i></a></h3>
+            <h3 class="content text-dark"><a href="638.php">Essex CR 638 <i class="text-body-tertiary">Valley St, Scotland Rd, High St</i></a></h3>
+            <h3 class="content text-dark"><a href="638s.php">Essex CR 638 Spur <i class="text-body-tertiary">Village Plz</i></a></h3>
+            <h3 class="content text-dark"><a href="639.php">Essex CR 639 <i class="text-body-tertiary">Grove Ave</i></a></h3>
+            <h3 class="content text-dark"><a href="640.php">Essex CR 640 <i class="text-body-tertiary">W Bradford Ave, E Bradford Ave</i></a></h3>
+            <h3 class="content text-dark"><a href="641.php">Essex CR 641 <i class="text-body-tertiary">Ridge Rd</i></a></h3>
+            <h3 class="content text-dark"><a href="642.php">Essex CR 642 <i class="text-body-tertiary">Hebron Rd</i></a></h3>
+            <h3 class="content text-dark"><a href="643.php">Essex CR 643 <i class="text-body-tertiary">Alexander Ave</i></a></h3>
+            <h3 class="content text-dark"><a href="644.php">Essex CR 644 <i class="text-body-tertiary">Kingsland St</i></a></h3>
+            <h3 class="content text-dark"><a href="644s.php">Essex CR 644 Spur <i class="text-body-tertiary">Cathedral Ave</i></a></h3>
+            <h3 class="content text-dark"><a href="645.php">Essex CR 645 <i class="text-body-tertiary">Franklin Ave</i></a></h3>
+            <h3 class="content text-dark"><a href="646.php">Essex CR 646 <i class="text-body-tertiary">Park Ave</i></a></h3>
+            <h3 class="content text-dark"><a href="647.php">Essex CR 647 <i class="text-body-tertiary">Union Ave</i></a></h3>
+            <h3 class="content text-dark"><a href="648.php">Essex CR 648 <i class="text-body-tertiary">Centre St</i></a></h3>
+            <h3 class="content text-dark"><a href="649.php">Essex CR 649 <i class="text-body-tertiary">John F. Kennedy Pkwy, S Livingston Ave</i></a></h3>
+            <h3 class="content text-dark"><a href="650.php">Essex CR 650 <i class="text-body-tertiary">High St, </i></a></h3>
+            <h3 class="content text-dark"><a href="651.php">Essex CR 651 <i class="text-body-tertiary">Hoover Ave</i></a></h3>
+            <h3 class="content text-dark"><a href="651s.php">Essex CR 651 Spur <i class="text-body-tertiary">E Passaic Ave</i></a></h3>
+            <h3 class="content text-dark"><a href="652.php">Essex CR 652 <i class="text-body-tertiary">JFK Dr, E Passaic Ave</i></a></h3>
+            <h3 class="content text-dark"><a href="653.php">Essex CR 653 <i class="text-body-tertiary">Ridgewood Ave</i></a></h3>
+            <h3 class="content text-dark"><a href="654.php">Essex CR 654 <i class="text-body-tertiary">Bay Ave</i></a></h3>
+            <h3 class="content text-dark"><a href="655.php">Essex CR 655 <i class="text-body-tertiary">Watchung Ave</i></a></h3>
+            <h3 class="content text-dark"><a href="656.php">Essex CR 656 <i class="text-body-tertiary">Harrison Ave</i></a></h3>
+            <h3 class="content text-dark"><a href="658.php">Essex CR 658 <i class="text-body-tertiary">Park Ave</i></a></h3>
+            <h3 class="content text-dark"><a href="659.php">Essex CR 659 <i class="text-body-tertiary">Main St</i></a></h3>
+            <h3 class="content text-dark"><a href="660.php">Essex CR 660 <i class="text-body-tertiary">Mount Pleasant Ave</i></a></h3>
+            <h3 class="content text-dark"><a href="661.php">Essex CR 661 <i class="text-body-tertiary">Beaufort Ave</i></a></h3>
+            <h3 class="content text-dark"><a href="661s.php">Essex CR 661 Spur <i class="text-body-tertiary">Old Beaufort Ave</i></a></h3>
+            <h3 class="content text-dark"><a href="662.php">Essex CR 662 <i class="text-body-tertiary">New Dutch Ln</i></a></h3>
+            <h3 class="content text-dark"><a href="663.php">Essex CR 663 <i class="text-body-tertiary">Broad St</i></a></h3>
+            <h3 class="content text-dark"><a href="664.php">Essex CR 664 <i class="text-body-tertiary">Coit St</i></a></h3>
+            <h3 class="content text-dark"><a href="665.php">Essex CR 665 <i class="text-body-tertiary">Irvington Ave, Clinton Ave</i></a></h3>
+            <h3 class="content text-dark"><a href="667.php">Essex CR 667 <i class="text-body-tertiary">Broadway</i></a></h3>
+            <h3 class="content text-dark"><a href="668.php">Essex CR 668 <i class="text-body-tertiary">Elm St</i></a></h3>
+            <h3 class="content text-dark"><a href="669.php">Essex CR 669 <i class="text-body-tertiary">Orange Rd</i></a></h3>
+            <h3 class="content text-dark"><a href="670.php">Essex CR 670 <i class="text-body-tertiary">Franklin St</i></a></h3>
+            <h3 class="content text-dark"><a href="671.php">Essex CR 671 <i class="text-body-tertiary">Washington St</i></a></h3>
+            <h3 class="content text-dark"><a href="672.php">Essex CR 672 <i class="text-body-tertiary">Mill St</i></a></h3>
+            <h3 class="content text-dark"><a href="672s.php">Essex CR 672 Spur <i class="text-body-tertiary">Bridge St</i></a></h3>
+            <h3 class="content text-dark"><a href="673.php">Essex CR 673 <i class="text-body-tertiary">Elm Rd</i></a></h3>
+            <h3 class="content text-dark"><a href="677.php">Essex CR 677 <i class="text-body-tertiary">Prospect Ave</i></a></h3>
             <h2 class="content text-primary"><a href="../cu/list.php">Previous Page (Cumberland County)</a></h2>
             <h2 class="content text-primary"><a href="../gl/list.php">Next Page (Gloucester County)</a></h2>
         </div>
