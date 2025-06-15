f6d0e7fd0050f1e92f7ca1abdb3b2dc6fca82b0d
farrelj5 <farrelj5@tcnj.edu>
2025-06-14 22:29:40 -0400

    started 195 #23

---
+++ pages/interstates/auxiliary/nj-195.php
@@ -0,0 +1,57 @@
+<?php include "../../../static/top.php"; ?>
+        <div class="col col-1 xxl"></div>
+        <div class="col col-11 xxl">
+            <h6 class="dir"><a href="../../../index.php">Home</a> > <a href="../list.php">US Highways</a> > US-1</h6>
+            <h1 class="header">Interstate 195 (New Jersey)</h1>
+            <h2 class="content text-dark">New Jersey Interstate 195 (I-195) is an east-west auxiliary interstate route covering central New Jersey. It is xxxx.xx mi (xxxx.xx km) long and is known as the Central Jersey Expressway.</h2>
+            <h4 class="content text-dark">Note: Mile and Kilometer measurements may not be exact.</h4>
+            <?php include "../../../static/table.php"; ?>
+                <tr>
+                    <td class="content text-dark small">0.00 mi</td>
+                    <td class="content text-dark small">0.00 km</td>
+                    <td class="content text-dark"><a href="../../state-highways/nj/29.php">NJ-29 (WB)</a> / <a href="295.php">I-295 (NB & SB)</a>, Hamilton Township, New Jersey 08610</td>
+                    <td class="content text-dark">Central Jersey Expy, Hamilton Township, 08610</td>
+                    <td class="content text-dark">Start of Route <br> Start Mercer County</td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">0.93 mi ( + 0.93 mi )</td>
+                    <td class="content text-dark small">1.49 km ( + 1.41 km )</td>
+                    <td class="content text-dark"><a href="../../us-highways/101-290/206.php">US-206 - Bordentown Rd</a>, Hamilton Township, New Jersey 08610</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">4.90 mi ( + 3.97 mi )</td>
+                    <td class="content text-dark small">7.88 km ( + 6.39 km )</td>
+                    <td class="content text-dark"><a href="../../us-highways/101-290/130.php">US-130 - Robbinsville Rd</a>, Hamilton Township, New Jersey 08610</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">5.58 mi ( + 0.68 mi )</td>
+                    <td class="content text-dark small">8.97 km ( + 1.09 km )</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark">Central Jersey Expy, Robbinsville Township, New Jersey 08691</td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">6.83 mi ( + 1.25 mi )</td>
+                    <td class="content text-dark small">10.98 km ( + 2.01 km )</td>
+                    <td class="content text-dark"><a href="../95.php">I-95 - New Jersey Tpke</a>, Robbinsville Township, New Jersey 08691</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">7.30 mi ( + 0.47 mi )</td>
+                    <td class="content text-dark small">11.74 km ( + 0.76 km )</td>
+                    <td class="content text-dark"><a href="../../county-roads/nj/526.php">Mercer CR 526 - Robbinsville Allentown Rd</a>, Robbinsville Township, New Jersey 08691</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">8.53 mi ( + 1.23 mi )</td>
+                    <td class="content text-dark small">13.73 km ( + 1.99 km )</td>
+                    <td class="content text-dark"><a href="../../county-roads/nj/539.php">Mercer CR 539 - Old York Rd</a> (concurrent with <a href="../../county-roads/nj/524.php">Mercer CR 524</a>), Robbinsville Township, New Jersey 08691</td>
+                    <td class="content text-dark">Central Jersey Expy, Upper Freehold, New Jersey 08501</td>
+                    <td class="content text-dark">End Mercer County, Start Monmouth County</td>
+                </tr>
