3a36be1ca67b675f0351a2c7c9c34437f93eef23
farrelj5 <farrelj5@tcnj.edu>
2025-06-17 13:06:59 -0400

    cr 43 and gloucester framework #26

---
+++ pages/county-roads/nj/mo/43.php
@@ -0,0 +1,37 @@
+<?php include "../../../../static/top.php"; ?>
+        <div class="col col-1 xxl"></div>
+        <div class="col col-11 xxl">
+            <h6 class="dir"><a href="../../../index.php">Home</a> > <a href="../list.php">County Roads in New Jersey</a> > <a href="list.php">County Roads in Monmouth County, New Jersey</a> > Monmouth CR 55</h6>
+            <h1 class="header">Monmouth County Route 55</h1>
+            <h2 class="content text-dark">New Jersey Monmouth County Route 55 (Monmouth CR 55) is a county road that connects US-9 and NJ-79. It is 5.04 mi (8.86 km).</h2>
+            <h4 class="content text-dark">Note: Mile and Kilometer measurements may not be exact.</h4>
+            
+            <?php include "../../../../static/table.php"; ?>
+                <tr>
+                    <td class="content text-dark small">0.00 mi</td>
+                    <td class="content text-dark small">0.00 km</td>
+                    <td class="content text-dark"><a href="../526.php">Monmouth CR 526 - Trenton Lakewood Rd</a>, Allentown, New Jersey 08501</td>
+                    <td class="content text-dark">Imlaystown Hightstown Rd, Allentown, New Jersey 08501</td>
+                    <td class="content text-dark">Start of Route</td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">0.71 mi ( + 0.71 mi )</td>
+                    <td class="content text-dark small">1.15 km ( + 1.15 km )</td>
+                    <td class="content text-dark"><a href="../../../interstates/auxiliary/nj-195.php">I-195 (WB) - Central Jersey Expy</a>, Allentown, New Jersey 08501</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">0.73 mi ( + 0.02 mi )</td>
+                    <td class="content text-dark small">1.18 km ( + 0.03 km )</td>
+                    <td class="content text-dark"><a href="../../../interstates/auxiliary/nj-195.php">I-195 (EB) - Central Jersey Expy</a>, Allentown, New Jersey 08501</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">1.01 mi ( + 0.28 mi )</td>
+                    <td class="content text-dark small">1.63 km ( + 0.45 km )</td>
+                    <td class="content text-dark"><a href="../524.php">Monmouth CR 524 - New Canton - Stone Tavern Rd</a>, Allentown, New Jersey 08501</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark">End of Route</td>
+                </tr>
