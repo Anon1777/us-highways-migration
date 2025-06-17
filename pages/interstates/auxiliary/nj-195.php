78fd632e548a41c060994dfaa2cf84d4de282765
farrelj5 <farrelj5@tcnj.edu>
2025-06-17 01:54:27 -0400

    195 & 138 & 88 #25

--- pages/interstates/auxiliary/nj-195.php
+++ pages/interstates/auxiliary/nj-195.php
@@ -1,9 +1,9 @@
 <?php include "../../../static/top.php"; ?>
         <div class="col col-1 xxl"></div>
         <div class="col col-11 xxl">
-            <h6 class="dir"><a href="../../../index.php">Home</a> > <a href="../list.php">US Highways</a> > US-1</h6>
+            <h6 class="dir"><a href="../../../index.php">Home</a> > <a href="../list.php">Interstates</a> > <a href="list.php">Auxiliary Routes</a> > I-195 (NJ)</h6>
             <h1 class="header">Interstate 195 (New Jersey)</h1>
-            <h2 class="content text-dark">New Jersey Interstate 195 (I-195) is an east-west auxiliary interstate route covering central New Jersey. It is xxxx.xx mi (xxxx.xx km) long and is known as the Central Jersey Expressway.</h2>
+            <h2 class="content text-dark">New Jersey Interstate 195 (I-195) is an east-west auxiliary interstate route covering central New Jersey. It is 34.09 mi (54.86 km) long and is known as the Central Jersey Expressway.</h2>
             <h4 class="content text-dark">Note: Mile and Kilometer measurements may not be exact.</h4>
             <?php include "../../../static/table.php"; ?>
                 <tr>
@@ -55,3 +55,102 @@
                     <td class="content text-dark">Central Jersey Expy, Upper Freehold, New Jersey 08501</td>
                     <td class="content text-dark">End Mercer County, Start Monmouth County</td>
                 </tr>
+                <tr>
+                    <td class="content text-dark small">11.77 mi ( + 3.24 mi )</td>
+                    <td class="content text-dark small">18.94 km ( + 5.21 km )</td>
+                    <td class="content text-dark"><a href="../../county-roads/nj/mo/43.php">Monmouth CR 43 - Imlaystown Hightstown Rd</a>, Upper Freehold, New Jersey 08501</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">14.13 mi ( + 2.36 mi )</td>
+                    <td class="content text-dark small">22.74 km ( + 3.80 km )</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark">Central Jersey Expy, Millstone, New Jersey 08510</td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">15.33 mi ( + 1.20 mi )</td>
+                    <td class="content text-dark small">24.67 km ( + 1.93 km )</td>
+                    <td class="content text-dark"><a href="../../county-roads/nj/526.php">Monmouth CR 526 - Trenton Lakewood Rd</a>, Millstone, New Jersey 08510</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">16.68 mi ( + 1.35 mi )</td>
+                    <td class="content text-dark small">26.84 km ( + 2.17 km )</td>
+                    <td class="content text-dark"><a href="../../county-roads/nj/537.php">Monmouth CR 537 - Monmouth Rd</a>, Millstone, New Jersey 08510</td>
+                    <td class="content text-dark">Central Jersey Expy, Jackson Township, New Jersey 08527</td>
+                    <td class="content text-dark">Pause Monmouth County, Start Ocean County</td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">17.62 mi ( + 0.94 mi )</td>
+                    <td class="content text-dark small">28.35 km ( + 1.51 km )</td>
+                    <td class="content text-dark"><a href="../../county-roads/nj/571.php">Ocean CR 571 - Cassville Rd</a> (concurrent with <a href="../../county-roads/nj/526.php">Ocean CR 526</a>), Jackson Township, New Jersey 08527</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">21.00 mi ( + 3.38 mi )</td>
+                    <td class="content text-dark small">33.78 km ( + 5.43 km )</td>
+                    <td class="content text-dark"><a href="../../county-roads/nj/527.php">Ocean CR 527 - Cedar Swamp Rd</a>, Jackson Township, New Jersey 08527</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">22.95 mi ( + 1.95 mi )</td>
+                    <td class="content text-dark small">36.92 km ( + 3.14 km )</td>
+                    <td class="content text-dark"><a href="../../county-roads/nj/oc/638.php">Ocean CR 638 - Jackson Mills Rd</a>, Jackson Township, New Jersey 08527</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">24.37 mi ( + 1.42 mi )</td>
+                    <td class="content text-dark small">39.21 km ( + 2.29 km )</td>
+                    <td class="content text-dark"><a href="../../county-roads/nj/oc/641.php">Ocean CR 641 - Chandler Rd</a>, Jackson Township, New Jersey 08527</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">25.57 mi ( + 1.20 mi )</td>
+                    <td class="content text-dark small">41.15 km ( + 1.94 km )</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark">Central Jersey Expy, Howell Township, New Jersey 07731</td>
+                    <td class="content text-dark">End Ocean County, Continue Monmouth County</td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">27.10 mi ( + 1.53 mi )</td>
+                    <td class="content text-dark small">43.62 km ( + 2.47 km )</td>
+                    <td class="content text-dark"><a href="../../us-highways/1-99/9.php">US-9</a>, Howell Township, New Jersey 07731</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">31.41 mi ( + 4.31 mi )</td>
+                    <td class="content text-dark small">50.56 km ( + 6.94 km )</td>
+                    <td class="content text-dark"><a href="../../county-roads/nj/547.php">Monmouth CR 547 (SB) - Lakewood Farmingdale Rd</a>, Howell Township, New Jersey 07731</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">31.49 mi ( + 0.08 mi )</td>
+                    <td class="content text-dark small">50.69 km ( + 0.13 km )</td>
+                    <td class="content text-dark"><a href="../../county-roads/nj/547.php">Monmouth CR 547 (NB) - Lakewood Farmingdale Rd</a>, Howell Township, New Jersey 07731</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">32.13 mi ( + 0.64 mi )</td>
+                    <td class="content text-dark small">51.71 km ( + 1.02 km )</td>
+                    <td class="content text-dark"><a href="../../county-roads/nj/524.php">Monmouth CR 524 - Allaire Rd</a>, Wall Township, New Jersey 07727</td>
+                    <td class="content text-dark">Central Jersey Expy, Wall Township, New Jersey 07727</td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">34.09 mi ( + 1.96 mi )</td>
+                    <td class="content text-dark small">54.86 km ( + 3.15 km )</td>
+                    <td class="content text-dark"><a href="../../state-highways/nj/34.php">NJ-34 (NB & SB)</a> / <a href="../../state-highways/nj/138.php">NJ-138 (EB)</a>, Wall Township, New Jersey 07727</td>
+                    <td class="content text-dark">Central Jersey Expy, Wall Township, New Jersey 07727</td>
+                    <td class="content text-dark">End of Route</td>
+                </tr>
+            <?php include "../../../static/bottom.php"; ?>
---
+++ pages/state-highways/nj/88.php
@@ -0,0 +1,94 @@
+<?php include "../../../static/top.php"; ?>
+        <div class="col col-1 xxl"></div>
+        <div class="col col-11 xxl">
+            <h6 class="dir"><a href="../../index.php">Home</a> > <a href="list.php">State Highways in New Jersey</a> > New Jersey Route 138</h6>
+            <h1 class="header">New Jersey Route 138</h1>
+            <h2 class="content text-dark">New Jersey Route 138 (NJ-138) is a state highway in eastern Monmouth County that finishes the eastern portion of the Central Jersey Expressway. It is 3.75 mi (6.03 km) long.</h2>
+            <h4 class="content text-dark">Note: Mile and Kilometer measurements may not be exact.</h4>
+            
+            <?php include "../../../static/table.php"; ?>
+                <tr>
+                    <td class="content text-dark small">0.00 mi</td>
+                    <td class="content text-dark small">0.00 km</td>
+                    <td class="content text-dark"><a href="../../us-highways/1-99/9.php">US-9 - Madison Ave</a> (concurrent with <a href="../../county-roads/nj/547.php">Monmouth CR 547</a>), Lakewood, New Jersey 08701</td>
+                    <td class="content text-dark">Main St, Lakewood, New Jersey 08701</td>
+                    <td class="content text-dark">Start of Route</td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">0.45 mi ( + 0.45 mi )</td>
+                    <td class="content text-dark small">0.72 km ( + 0.72 km )</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark">Ocean Ave, Lakewood, New Jersey 08701</td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">2.16 mi ( + 1.71 mi )</td>
+                    <td class="content text-dark small">3.47 km ( + 2.75 km )</td>
+                    <td class="content text-dark"><a href="../../county-roads/nj/oc/623.php">Ocean CR 623 - New Hampshire Ave</a>, Lakewood, New Jersey 08701</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">3.24 mi ( + 1.08 mi )</td>
+                    <td class="content text-dark small">5.21 km ( + 1.74 km )</td>
+                    <td class="content text-dark"><a href="../../county-roads/nj/549.php">Ocean CR 549 - Lanes Mill Rd (NB) / Chambers Bridge Rd (SB)</a>, Lakewood, New Jersey 08701</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">3.43 mi ( + 0.03 mi )</td>
+                    <td class="content text-dark small">5.51 km ( + 0.04 km )</td>
+                    <td class="content text-dark"><a href="444.php">NJ-444 (NB) - Garden State Pkwy</a>, Lakewood, New Jersey 08701</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">3.43 mi ( + 0.03 mi )</td>
+                    <td class="content text-dark small">5.51 km ( + 0.04 km )</td>
+                    <td class="content text-dark"><a href="444.php">NJ-444 (NB) - Garden State Pkwy</a>, Lakewood, New Jersey 08701</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">3.55 mi ( + 0.12 mi )</td>
+                    <td class="content text-dark small">5.71 km ( + 0.20 km )</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark">NJ-88, Brick Township, New Jersey 08724</td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">5.19 mi ( + 1.64 mi )</td>
+                    <td class="content text-dark small">8.36 km ( + 2.65 km )</td>
+                    <td class="content text-dark"><a href="70.php">NJ-70</a>, Brick Township, New Jersey 08724</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">7.29 mi ( + 2.10 mi )</td>
+                    <td class="content text-dark small">11.74 km ( + 3.38 km )</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark">Ocean Rd, Brick Township, New Jersey 08724</td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">7.37 mi ( + 0.08 mi )</td>
+                    <td class="content text-dark small">11.86 km ( + 0.12 km )</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark">Ocean Rd, Point Pleasant, New Jersey 08742</td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">7.37 mi ( + 0.08 mi )</td>
+                    <td class="content text-dark small">11.86 km ( + 0.12 km )</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark">Ocean Rd, Point Pleasant, New Jersey 08742</td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">9.79 mi ( + 2.42 mi )</td>
+                    <td class="content text-dark small">15.76 km ( + 3.90 km )</td>
+                    <td class="content text-dark"><a href="35.php">NJ-35 - Sea Ave</a>, Point Pleasant Beach, New Jersey 08742</td>
+                    <td class="content text-dark">Ocean Rd, Point Pleasant Beach, New Jersey 08742</td>
+                    <td class="content text-dark">End of Route</td>
+                </tr>
+            <?php include "../../../static/bottom.php"; ?>
