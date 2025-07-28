9694aeaccba0625815c7ed8db4a0b4aa882162a9
farrelj5 <farrelj5@tcnj.edu>
2025-07-28 00:37:02 -0400

    number #30

--- pages/state-highways/nj/440.php
+++ pages/state-highways/nj/440.php
@@ -3,7 +3,7 @@
         <div class="col col-11 xxl">
             <h6 class="dir"><a href="../../index.php">Home</a> > <a href="list.php">State Highways in New Jersey</a> > New Jersey Route 29</h6>
             <h1 class="header">New Jersey Route 440</h1>
-            <h2 class="content text-dark">New Jersey Route 440 (NJ-440) is a state highway in north New Jersey that connects Bayonne to Staten Island. It is 4.10 mi (6.58 km) long.</h2>
+            <h2 class="content text-dark">New Jersey Route 440 (NJ-440) is a state highway in north New Jersey that connects Bayonne to Staten Island. It is 4.60 mi (7.42 km) long.</h2>
             <h4 class="content text-dark">Note: Mile and Kilometer measurements may not be exact.</h4>
             
             <?php include "../../../static/table.php"; ?>
@@ -18,6 +18,153 @@
                     <td class="content text-dark small">0.32 mi ( + 0.32 mi )</td>
                     <td class="content text-dark small">0.51 km ( + 0.51 km )</td>
                     <td class="content text-dark"><a href="../../county-roads/nj/514.php">Middlesex CR 514 - Woodbridge Ave</a>, Edison, New Jersey 08817</td>
-                    <td class="content text-dark">NJ-440, Edison, New Jersey 08817</td>
-                    <td class="content text-dark">Start of Route // Start Route in New Jersey // Start Middlesex County</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">0.96 mi ( + 0.64 mi )</td>
+                    <td class="content text-dark small">1.54 km ( + 1.03 km )</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark">NJ-440, Woodbridge Township, New Jersey 07095</td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">1.44 mi ( + 0.48 mi )</td>
+                    <td class="content text-dark small">2.32 km ( + 0.78 km )</td>
+                    <td class="content text-dark"><a href="../../county-roads/nj/mi/656.php">Middlesex CR 656 - Smith St</a>, Woodbridge Township, New Jersey 07095</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">2.00 mi ( + 0.56 mi )</td>
+                    <td class="content text-dark small">3.23 km ( + 0.91 km )</td>
+                    <td class="content text-dark"><a href="444.php">NJ-444 - Garden State Pkwy</a>, Woodbridge Township, New Jersey 07095</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">2.05 mi ( + 0.05 mi )</td>
+                    <td class="content text-dark small">3.31 km ( + 0.08 km )</td>
+                    <td class="content text-dark"><a href="../../us-highways/1-98/9.php">US-9</a>, Woodbridge Township, New Jersey 07095</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">2.05 mi ( + 0.57 mi )</td>
+                    <td class="content text-dark small">3.31 km ( + 0.08 km )</td>
+                    <td class="content text-dark"><a href="../../county-roads/nj/mi/616.php">Middlesex CR 616 - New Brunswick Ave</a>, Woodbridge Township, New Jersey 07095</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">2.14 mi ( + 0.09 mi )</td>
+                    <td class="content text-dark small">3.46 km ( + 0.15 km )</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark">NJ-440, Perth Amboy, New Jersey 08861</td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">2.21 mi ( + 0.07 mi )</td>
+                    <td class="content text-dark small">3.58 km ( + 0.12 km )</td>
+                    <td class="content text-dark"><a href="../../county-roads/nj/mi/655.php">Middlesex CR 655 - S Florida Grove Rd</a>, Perth Amboy, New Jersey 08861</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">2.63 mi ( + 0.42 mi )</td>
+                    <td class="content text-dark small">4.26 km ( + 0.68 km )</td>
+                    <td class="content text-dark"><a href="184.php">NJ-184 - Pfeiffer Blvd (EB)</a> (concurrent with <a href="../../county-roads/nj/501.php">Middlesex CR 501 - Pfeiffer Blvd (EB)</a>), Perth Amboy, New Jersey 08861</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">2.80 mi ( + 0.17 mi )</td>
+                    <td class="content text-dark small">4.53 km ( + 0.27 km )</td>
+                    <td class="content text-dark"><a href="35.php">NJ-35 - Convery Blvd</a>, Perth Amboy, New Jersey 08861</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">2.83 mi ( + 0.03 mi )</td>
+                    <td class="content text-dark small">4.58 km ( + 0.05 km )</td>
+                    <td class="content text-dark"><a href="184.php">NJ-184 - Pfeiffer Blvd (EB)</a> (concurrent with <a href="../../county-roads/nj/501.php">Middlesex CR 501 - Pfeiffer Blvd (EB)</a>), Perth Amboy, New Jersey 08861</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark">Start concurrency with Middlesex CR 501</td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">3.07 mi ( + 0.24 mi )</td>
+                    <td class="content text-dark small">4.96 km ( + 0.38 km )</td>
+                    <td class="content text-dark"><a href="../../county-roads/nj/mi/653.php">Middlesex CR 653 - Amboy Ave</a>, Perth Amboy, New Jersey 08861</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">3.34 mi ( + 0.27 mi )</td>
+                    <td class="content text-dark small">5.39 km ( + 0.43 km )</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark">Outerbridge Xing, Perth Amboy, New Jersey 08861</td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">3.66 mi ( + 0.32 mi )</td>
+                    <td class="content text-dark small">5.90 km ( + 0.51 km )</td>
+                    <td class="content text-dark"><a href="../../county-roads/nj/mi/611.php">Middlesex CR 611 - State St</a>, Perth Amboy, New Jersey 08861</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">4.60 mi ( + 0.94 mi )</td>
+                    <td class="content text-dark small">7.42 km ( + 1.52 km )</td>
+                    <td class="content text-dark"><a href="../ny/440.php">NY-440 - Outerbridge Xing</a>, New York, New York, 10314</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark">Pause Route in New Jersey // End Middlesex County // End concurrency with Middlesex CR 501 // Gap in Route (See <a href="../ny/440.php">NY-440</a>)</td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">16.37 mi ( + 11.77 mi )</td>
+                    <td class="content text-dark small">26.98 km ( + 19.56 km )</td>
+                    <td class="content text-dark"><a href="../ny/440.php">NY-440 - Willowbrook Expy</a>, New York, New York, 10314</td>
+                    <td class="content text-dark">NJ-440, Bayonne, New Jersey 07002</td>
+                    <td class="content text-dark">Resume Route in New Jersey // Start Hudson County // Start concurrency with Hudson CR 501</td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">16.72 mi ( + 0.35 mi )</td>
+                    <td class="content text-dark small">27.55 km ( + 0.57 km )</td>
+                    <td class="content text-dark"><a href="../../county-roads/nj/hu/700.php">Hudson CR 735 - W First St</a>, Bayonne, New Jersey, 07002</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">16.84 mi ( + 0.12 mi )</td>
+                    <td class="content text-dark small">27.74 km ( + 0.19 km )</td>
+                    <td class="content text-dark"><a href="../../county-roads/nj/hu/700.php">Hudson CR 700 - Gertrude St</a>, Bayonne, New Jersey, 07002</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">16.90 mi ( + 0.06 mi )</td>
+                    <td class="content text-dark small">27.83 km ( + 0.09 km )</td>
+                    <td class="content text-dark"><a href="../../county-roads/nj/hu/701.php">Hudson CR 701 - W Third St</a>, Bayonne, New Jersey, 07002</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">16.90 mi ( + 0.06 mi )</td>
+                    <td class="content text-dark small">27.93 km ( + 0.10 km )</td>
+                    <td class="content text-dark"><a href="../../county-roads/nj/hu/702.php">Hudson CR 702 - Juliette St</a>, Bayonne, New Jersey, 07002</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">17.01 mi ( + 0.11 mi )</td>
+                    <td class="content text-dark small">28.10 km ( + 0.17 km )</td>
+                    <td class="content text-dark"><a href="../../county-roads/nj/501.php">Hudson CR 501 - John F. Kennedy Blvd</a>, Bayonne, New Jersey, 07002</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark">End concurrency with Hudson CR 501</td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">17.01 mi ( + 0.11 mi )</td>
+                    <td class="content text-dark small">28.10 km ( + 0.17 km )</td>
+                    <td class="content text-dark"><a href="../../county-roads/nj/501.php">Hudson CR 501 - John F. Kennedy Blvd</a>, Bayonne, New Jersey, 07002</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark">End concurrency with Hudson CR 501</td>
                 </tr>
---
+++ pages/state-highways/ny/440.php
@@ -0,0 +1,51 @@
+<?php include "../../../static/top.php"; ?>
+        <div class="col col-1 xxl"></div>
+        <div class="col col-11 xxl">
+            <h6 class="dir"><a href="../../index.php">Home</a> > <a href="list.php">State Highways in New Jersey</a> > New Jersey Route 29</h6>
+            <h1 class="header">New York Route 440</h1>
+            <h2 class="content text-dark">New York Route 440 (NY-440) is a state highway in Staten Island, New York. It is 4.10 mi (6.58 km) long.</h2>
+            <h4 class="content text-dark">Note: Mile and Kilometer measurements may not be exact.</h4>
+            
+            <?php include "../../../static/table.php"; ?>
+                <tr>
+                    <td class="content text-dark small">0.00 mi</td>
+                    <td class="content text-dark small">0.00 km</td>
+                    <td class="content text-dark"><a href="../nj/440.php">NJ-440</a>, Perth Amboy, New Jersey 08861</td>
+                    <td class="content text-dark">Outerbridge Xing, New York, New York 10314</td>
+                    <td class="content text-dark">Start of Route</td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">1.07 mi ( + 1.07 mi )</td>
+                    <td class="content text-dark small">1.73 km ( + 1.73 km )</td>
+                    <td class="content text-dark"><a href="909C.php">NY-909C - Korean War Veterans Pkwy</a>, New York, New York 10314</td>
+                    <td class="content text-dark">W Shore Expy, New York, New York 10314</td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">7.79 mi ( + 7.72 mi )</td>
+                    <td class="content text-dark small">13.15 km ( + 12.42 km )</td>
+                    <td class="content text-dark"><a href="../../interstates/auxiliary/ny-278.php">I-278 - Staten Island Expy</a>, New York, New York 10314</td>
+                    <td class="content text-dark">Staten Island Expy, New York, New York 10314</td>
+                    <td class="content text-dark">Start concurrency with I-278</td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">9.34 mi ( + 1.55 mi )</td>
+                    <td class="content text-dark small">15.65 km ( + 2.50 km )</td>
+                    <td class="content text-dark">NY-440 - Dr. Martin Luther King, Jr. Expy, New York, New York 10314</td>
+                    <td class="content text-dark">Dr. Martin Luther King, Jr. Expy, New York, New York 10314</td>
+                    <td class="content text-dark">End concurrency with I-278</td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">11.00 mi ( + 1.66 mi )</td>
+                    <td class="content text-dark small">18.32 km ( + 2.67 km )</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark">Willowbrook Expy, New York, New York 10314</td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">11.77 mi ( + 0.77 mi )</td>
+                    <td class="content text-dark small">19.56 km ( + 1.24 km )</td>
+                    <td class="content text-dark"><a href="../nj/440.php">NJ-440</a>, Bayonne, New Jersey 07002</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark">End of Route</td>
+                </tr>
