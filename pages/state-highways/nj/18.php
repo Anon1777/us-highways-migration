5f7e159098cf657b04e083f99cbba0540d5d4e03
farrelj5 <farrelj5@tcnj.edu>
2025-08-27 18:28:42 -0400

    18 and 64

---
+++ pages/state-highways/nj/18.php
@@ -0,0 +1,73 @@
+<?php include "../../../static/top.php"; ?>
+        <div class="col col-1 xxl"></div>
+        <div class="col col-11 xxl">
+            <h6 class="dir"><a href="../../index.php">Home</a> > <a href="list.php">State Highways in New Jersey</a> > New Jersey Route 18</h6>
+            <h1 class="header">New Jersey Route 18</h1>
+            <h2 class="content text-dark">New Jersey Route 18 (NJ-18) is a state highway in New Jersey that connects NJ-138 in Wall Township to I-295 in Piscataway. It is 0.41 mi (0.65 km) long.</h2>
+            <h4 class="content text-dark">Note: Mile and Kilometer measurements may not be exact.</h4>
+            
+            <?php include "../../../static/table.php"; ?>
+                <tr>
+                    <td class="content text-dark small">0.00 mi</td>
+                    <td class="content text-dark small">0.00 km</td>
+                    <td class="content text-dark"><a href="138.php">NJ-138</a>, Wall Township, New Jersey 07719</td>
+                    <td class="content text-dark">NJ-18, Wall Township, New Jersey 07712</td>
+                    <td class="content text-dark">Start of Route</td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">0.29 mi ( + 0.29 mi )</td>
+                    <td class="content text-dark small">0.47 km ( + 0.47 km )</td>
+                    <td class="content text-dark"><a href="138.php">NJ-138</a>, Wall Township, New Jersey 07719</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">1.15 mi ( + 0.86 mi )</td>
+                    <td class="content text-dark small">1.85 km ( + 1.38 km )</td>
+                    <td class="content text-dark"><a href="../../county-roads/nj/mo/18.php">Monmouth CR 18 - Belmar Blvd</a>, Wall Township, New Jersey 07719</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">2.19 mi ( + 1.04 mi )</td>
+                    <td class="content text-dark small">3.53 km ( + 1.68 km )</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark">NJ-18, Neptune Township, New Jersey 07753</td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">3.20 mi ( + 1.01 mi )</td>
+                    <td class="content text-dark small">5.15 km ( + 1.62 km )</td>
+                    <td class="content text-dark"><a href="33.php">NJ-33 - Corlies Ave</a>, Neptune Township, New Jersey 07753</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">4.12 mi ( + 0.92 mi )</td>
+                    <td class="content text-dark small">6.63 km ( + 1.48 km )</td>
+                    <td class="content text-dark"><a href="../../county-roads/nj/mo/17.php">Monmouth CR 17 - W Bangs Ave</a>, Neptune Township, New Jersey 07753</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">4.75 mi ( + 0.63 mi )</td>
+                    <td class="content text-dark small">7.65 km ( + 1.02 km )</td>
+                    <td class="content text-dark"><a href="66.php">NJ-66 - Asbury Ave</a> (concurrent with <a href="../../county-roads/nj/mo/16.php">Monmouth CR 16</a>), Neptune Township, New Jersey 07753</td>
+                    <td class="content text-dark">NJ-18, Ocean Township, New Jersey 07712</td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">6.98 mi ( + 2.23 mi )</td>
+                    <td class="content text-dark small">11.24 km ( + 3.59 km )</td>
+                    <td class="content text-dark"><a href="../../county-roads/nj/mo/14.php">Monmouth CR 14 - W Park Ave</a>, Ocean Township, New Jersey 07712</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">7.93 mi ( + 0.95 mi )</td>
+                    <td class="content text-dark small">12.76 km ( + 1.52 km )</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark">NJ-18, Eatontown, New Jersey 07724</td>
+                    <td class="content text-dark"></td>
+                </tr>
+                
---
+++ pages/state-highways/nj/64.php
@@ -0,0 +1,31 @@
+<?php include "../../../static/top.php"; ?>
+        <div class="col col-1 xxl"></div>
+        <div class="col col-11 xxl">
+            <h6 class="dir"><a href="../../index.php">Home</a> > <a href="list.php">State Highways in New Jersey</a> > New Jersey Route 64</h6>
+            <h1 class="header">New Jersey Route 64</h1>
+            <h2 class="content text-dark">New Jersey Route 64 (NJ-64) is a very short state highway in west New Jersey that exists to consolidate the roads Mercer CR 571 and Mercer CR 526. It is 0.41 mi (0.65 km) long.</h2>
+            <h4 class="content text-dark">Note: Mile and Kilometer measurements may not be exact.</h4>
+            
+            <?php include "../../../static/table.php"; ?>
+                <tr>
+                    <td class="content text-dark small">0.00 mi</td>
+                    <td class="content text-dark small">0.00 km</td>
+                    <td class="content text-dark"><a href="../../county-roads/nj/me/615.php">Mercer CR 615 (EB & WB) - Cranbury Rd</a> / <a href="../../county-roads/nj/526.php">Mercer CR 526 (NB & SB) - Princeton Hightstown Rd</a> (concurrent with <a href="../../county-roads/nj/571.php">Mercer CR 571</a>), West Windsor Township, New Jersey 08550</td>
+                    <td class="content text-dark">NJ-64, West Windsor Township, New Jersey 08550</td>
+                    <td class="content text-dark">Start of Route // Start concurrency with Mercer CR 526 & Mercer CR 571</td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">0.33 mi ( + 0.33 mi )</td>
+                    <td class="content text-dark small">0.53 km ( + 0.53 km )</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark">Washington Rd, West Windsor Township, New Jersey 08550</td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">0.41 mi ( + 0.08 mi )</td>
+                    <td class="content text-dark small">0.65 km ( + 0.12 km )</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark">End of Route</td>
+                </tr>
+            <?php include "../../../static/bottom.php"; ?>
