6404eee052472e9ee97ca16b74ce6ab8c8a41b7d
farrelj5 <farrelj5@tcnj.edu>
2025-09-05 10:50:23 -0400

    378 & 528 #40

---
+++ pages/us-highways/101-290/206.php
@@ -0,0 +1,35 @@
+<?php include "../../../static/top.php"; ?>
+        
+            <h6 class="dir"><a href="../../index.php">Home</a> > <a href="../list.php">US Highways</a> > US-206</h6>
+            <h1 class="header" id="us9">United States Route 206</h1>
+            <h2 class="content text-dark">United States Route 206 (US-206) is a north-south route covering a little bit of Delaware through the whole of New York. It is xxx.xx mi (xxx.xx km) long.</h2>
+            <h4 class="content text-dark">Note: Mile and Kilometer measurements may not be exact.</h4>
+            <?php include "../../../static/table.php"; ?>
+                <tr>
+                    <td class="content text-dark small">0.00 mi</td>
+                    <td class="content text-dark small">0.00 km</td>
+                    <td class="content text-dark"><a href="../1-98/30.php">US-30 - N White Horse Pk (WB / EB)</a> / <a href="../../state-highways/nj/54.php">NJ-54 - Bellevue Ave</a>, Hammonton, New Jersey 08037</td>
+                    <td class="content text-dark">Trenton Rd, Hammonton, New Jersey 08037</td>
+                    <td class="content text-dark">Start of Route // Start of Route in New Jersey // Start Atkantic County</td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">1.25 mi ( + 1.25 mi )</td>
+                    <td class="content text-dark small">2.01 km ( + 2.01 km )</td>
+                    <td class="content text-dark"><a href="../../county-roads/nj/at/693.php">Atlantic CR 693 - Columbia Rd</a>, Hammonton, New Jersey 08037</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">2.98 mi ( + 1.73 mi )</td>
+                    <td class="content text-dark small">4.79 km ( + 2.78 km )</td>
+                    <td class="content text-dark"><a href="../../county-roads/nj/at/613.php">Atlantic CR 613 - Middle Rd</a>, Hammonton, New Jersey 08037</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark"></td>
+                </tr>
+                <tr>
+                    <td class="content text-dark small">5.99 mi ( + 3.31 mi )</td>
+                    <td class="content text-dark small">10.12 km ( + 5.33 km )</td>
+                    <td class="content text-dark"></td>
+                    <td class="content text-dark">US-206, Shamong, New Jersey 08088</td>
+                    <td class="content text-dark">End Atlantic County, Start Burlington County</td>
+                </tr>
