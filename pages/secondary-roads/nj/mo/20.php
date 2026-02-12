4451f250f7b620c3e1147726679cbeb71d760cda
farrelj5 <farrelj5@tcnj.edu>
2026-02-12 12:21:53 -0500

    ae

---
+++ pages/secondary-roads/nj/mo/20.php
@@ -0,0 +1,15 @@
+<?php include "../../../../static/top.php"; ?>
+        
+            <h6 class="dir"><a href="../../../index-redirect.php">Home</a> > <a href="../list.php">County Roads in New Jersey</a> > <a href="list.php">County Roads in Monmouth County, New Jersey</a> > Monmouth CR 20</h6>
+            <h1 class="header">Monmouth County Route 20</h1>
+            <h2 class="content text-dark">New Jersey Monmouth County Route 20 (Monmouth CR 20) is a county road that connects NJ-79 to other roads closer to NJ-33 and NJ-34. It is 4.94 mi (7.95 km) long.</h2>
+            <h4 class="content text-dark">Note: Mile and Kilometer measurements may not be exact.</h4>
+            
+            <?php include "../../../../static/table.php"; ?>
+                <tr>
+                    <td class="content text-dark small">0.00 mi</td>
+                    <td class="content text-dark small">0.00 km</td>
+                    <td class="content text-dark"><a href="../../../state-highways/nj/79.php">NJ-79 - Broadway Rd</a>, Freehold, New Jersey 07728</td>
+                    <td class="content text-dark">Dutch Lane Rd, Freehold, New Jersey 07728</td>
+                    <td class="content text-dark">Start of Route</td>
+                </tr>
